65f0c06ec8ff73cbff0fc79e7d5912b9c252b6b1
guillermo-gordon18-2000 <guillermo18zgordon@gmail.com>
2022-10-11 17:05:08 -0500

    Version V.1.4.6

--- View/Main.php
+++ View/Main.php
@@ -6,6 +6,7 @@
       <meta name="viewport" content="width=device-width, initial-scale=1.0">
       <title>Prototipo</title>
      <!-- MATERIAL ICONS -->
+     <link href='https://unpkg.com/boxicons@2.0.9/css/boxicons.min.css' rel='stylesheet'>
      <link href="https://fonts.googleapis.com/icon?family=Material+Icons+Sharp" rel="stylesheet">
       <!-- STYLESHEET -->
        <link rel="stylesheet" href="Public/css/style-Main.css">  
